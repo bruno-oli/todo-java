6d2188ad0b3739c950046f9908dc6bfc0396a302
bruno-oli <brunomaz177@gmail.com>
2023-10-09 14:43:47 -0300

    user controller

---
+++ src/main/java/br/com/brunomax/todolist/user/UserController.java
@@ -0,0 +1,15 @@
+package br.com.brunomax.todolist.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+  @PostMapping("/")
+  public UserModel create(@RequestBody UserModel user) {
+    return user;
+  }
+}
