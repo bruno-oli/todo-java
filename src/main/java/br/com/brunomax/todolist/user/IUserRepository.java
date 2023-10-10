90b4024b710a9e6976bce1913388f423ab9cee03
bruno-oli <brunomaz177@gmail.com>
2023-10-10 18:02:01 -0300

    create user route

---
+++ src/main/java/br/com/brunomax/todolist/user/IUserRepository.java
@@ -0,0 +1,9 @@
+package br.com.brunomax.todolist.user;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IUserRepository extends JpaRepository<UserModel, UUID> {
+  UserModel findByUsername(String username);
+}
--- src/main/java/br/com/brunomax/todolist/user/UserController.java
+++ src/main/java/br/com/brunomax/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.brunomax.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+  @Autowired
+  private IUserRepository userRepository;
   @PostMapping("/")
-  public UserModel create(@RequestBody UserModel user) {
-    return user;
+  public ResponseEntity create(@RequestBody UserModel userModel) {
+    var userExists = this.userRepository.findByUsername(userModel.getUsername());
+
+    if (userExists != null) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists");
+    }
+
+    var createdUser = this.userRepository.save(userModel);
+
+    return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
   }
+
 }
