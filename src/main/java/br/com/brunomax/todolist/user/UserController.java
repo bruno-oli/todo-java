6f31e44730adf01e7afa11940a789c7854d95239
bruno-oli <brunomaz177@gmail.com>
2023-10-11 10:40:40 -0300

    add hash in user password

--- src/main/java/br/com/brunomax/todolist/user/UserController.java
+++ src/main/java/br/com/brunomax/todolist/user/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -21,6 +23,9 @@ public class UserController {
     if (userExists != null) {
       return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists");
     }
+
+    var hashPassword = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+    userModel.setPassword(hashPassword);
 
     var createdUser = this.userRepository.save(userModel);
 
