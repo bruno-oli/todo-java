91cba89e68bfaa7bfe26a53f10414a5182c119ee
bruno-oli <brunomaz177@gmail.com>
2023-10-13 10:35:41 -0300

    find all tasks by user id

--- src/main/java/br/com/brunomax/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/brunomax/todolist/filter/FilterTaskAuth.java
@@ -20,8 +20,7 @@ public class FilterTaskAuth extends OncePerRequestFilter {
     private IUserRepository userRepository;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-
+    protected void doFilterInternal(@org.jetbrains.annotations.NotNull HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var servletPath = request.getServletPath();
 
         if (servletPath.equals("/tasks/")) {
--- src/main/java/br/com/brunomax/todolist/task/TaskController.java
+++ src/main/java/br/com/brunomax/todolist/task/TaskController.java
@@ -2,11 +2,12 @@ package br.com.brunomax.todolist.task;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -16,12 +17,30 @@ public class TaskController {
     private ITaskRepository taskRepository;
 
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
+    public ResponseEntity<Object> create(@RequestBody TaskModel taskModel, HttpServletRequest request) {
         var userId = request.getAttribute("userId");
-        System.out.println(userId);
+
+        var currentData = LocalDateTime.now();
+        if (currentData.isAfter(taskModel.getStartAt()) || currentData.isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The start/end date must be greater than the current date!");
+        }
+
+        if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The start date must be less than the end date!");
+        }
 
         taskModel.setUserId((UUID) userId);
 
-        return this.taskRepository.save(taskModel);
+        var task = this.taskRepository.save(taskModel);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(task);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<TaskModel>> list(HttpServletRequest request) {
+        var userId = request.getAttribute("userId");
+        var tasks = this.taskRepository.findByUserId((UUID) userId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(tasks);
     }
 }
